408cf721e0013212179b26946456701b585e18b0
Sebastian Zanabria <zanabria.s@pucp.pe>
2023-10-02 03:03:29 +0000

    migrate local state to terraform cloud using terraform cloud block

--- main.tf
+++ main.tf
@@ -1,4 +1,21 @@
 terraform {
+  # backend "remote" {
+  #   hostname = "app.terraform.io"
+  #   organization = "personal-seba-zeta"
+
+  #   workspaces {
+  #     name = "terra-house-1"
+  #   }
+  # }
+
+  cloud {
+    organization = "personal-seba-zeta"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
+
   required_providers {
     random = {
       source = "hashicorp/random"
