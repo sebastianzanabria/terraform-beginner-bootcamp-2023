ab559a1b47861103545fc5635fb16992fab54f7e
Sebastian Zanabria <zanabria.s@pucp.pe>
2023-10-04 05:44:13 +0000

    restructure root module

--- main.tf
+++ main.tf
@@ -1,41 +1,3 @@
-terraform {
-  # backend "remote" {
-  #   hostname = "app.terraform.io"
-  #   organization = "personal-seba-zeta"
-
-  #   workspaces {
-  #     name = "terra-house-1"
-  #   }
-  # }
-
-  cloud {
-    organization = "personal-seba-zeta"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.19.0"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
-}
-
-provider "random" {
-  # Configuration options
-}
-
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   lower     = true
@@ -49,9 +11,8 @@ resource "aws_s3_bucket" "example" {
   # Bucket Naming Rules
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
   bucket = random_string.bucket_name.result
-}
-
-
-output "random_bucket_name" {
-    value = random_string.bucket_name.result
+  
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
