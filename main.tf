aa7c52bfff33a5f4f4827abad72db00aa9bcdd89
Sebastian Zanabria <zanabria.s@pucp.pe>
2023-10-07 05:29:21 +0000

    enabled static website hosting, uploaded index and error file via terraform

--- main.tf
+++ main.tf
@@ -21,4 +21,6 @@ module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
   bucket_name = var.bucket_name
   user_uuid = var.user_uuid
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
 }
